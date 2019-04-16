fcfe1d1671fac2c95a47fd504d07e5b5a5197615
Emma Dedmond <emmadedmond@yahoo.com>
2019-04-16 19:17:09 -0400

    basic setup

---
+++ server.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require("express");
+const exphbs = require("express-handlebars");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3333;
+
+const app = express();
+const db = require("./models");
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+app.use(express.static('public'));
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI);
+
+app.use(require('./routes/htmlRoutes')(db));
+app.use('/api', require('./routes/apiRoutes')(db));
+
+app.listen(PORT, () => {
+    console.log(`App listening on port: ${PORT}`);
+});
